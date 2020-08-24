70533b6125250bd5cd1f9dda8126da43103348a7
judah-levi <kolamiti92@gmail.com>
2020-08-24 12:47:13 +0300

    updated root message

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to SpendWell's API"}
+    return {"message": "Welcome to SpendWell's API, feel free to visit the docs for more info on how to use me! https://spendwell-api.herokuapp.com/docs"}
 
 @app.post("/barcode")
 async def post_barcode(product: Product):
