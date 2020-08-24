37b63400fe493eceecb07be6786b02620716ceb4
judah-levi <kolamiti92@gmail.com>
2020-08-24 09:15:49 +0300

    setup api.py

--- api.py
+++ api.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+import os
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to SpendWell's API"}
+
+@app.post("/barcode")
+async def post_barcode():
+    data = request.get_json()
+
+
+if __name__ == '__main__':
+    if os.environ.get('PORT') is not None:
+        app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT'))
+    else:
+        app.run(debug=True, host='0.0.0.0') 
