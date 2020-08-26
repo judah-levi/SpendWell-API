7aa814fc7a22003580d915360632cf7e0443cf29
arstrim <arstrim97@gmail.com>
2020-08-26 11:12:40 +0300

    returning before nutriscore

--- app/models/reccomendation_engine.py
+++ app/models/reccomendation_engine.py
@@ -4,13 +4,23 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
+# DATABASE_URL = 'postgres://qcsfswwzpmpnoe:c79186b0b2d5cdb40b41ff40d914a4d2c0f8285529274f16fad2384fcb1a9465@ec2-54-247-103-43.eu-west-1.compute.amazonaws.com:5432/d9nkgr3sa7opsa'
 
 
 class ReccomendationEngine():
     @staticmethod
     def get_rec_list(barcode):
-        print(DATABASE_URL)
         conn = psycopg2.connect(DATABASE_URL)
+        columns = ['url', 'image_url', 'nutriscore_grade', 'label', 'barcode', 'category',
+                   'subcategory', 'nutriscore', 'name', 'brand', 'ingredients']
+
+        with conn.cursor() as cur:
+            cur.execute('SELECT * FROM products2 WHERE barcode = %s;', [barcode])
+            prev_result = cur.fetchone()
+        prev_prod = {}
+        for i, col in enumerate(columns):
+            prev_prod[col] = prev_result[i + 1]
+
         with conn.cursor() as cur:
             cur.execute('SELECT label FROM products2 WHERE barcode = %s;', [barcode])
             result = cur.fetchall()
@@ -18,12 +28,14 @@ class ReccomendationEngine():
             result = cur.fetchall()
 
         list_of_dicts = []
-        columns = ['url', 'image_url', 'nutriscore_grade', 'label', 'barcode', 'category',
-       'subcategory', 'nutriscore', 'name', 'brand', 'ingredients']
         for j in range(len(result)):
-            res_dict = {}
+            res_dict = {'before_nutriscore':prev_prod['nutriscore']}
             for i, col in enumerate(columns):
                 res_dict[col] = result[j][i+1]
             list_of_dicts.append(res_dict)
 
         return list_of_dicts
+
+print(ReccomendationEngine.get_rec_list(1938067)[0])
+print(ReccomendationEngine.get_rec_list(1938067)[1])
+print(ReccomendationEngine.get_rec_list(1938067)[2])
